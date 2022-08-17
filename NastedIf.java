fec9c630c50bad0f4a86efbb3a3850d56542e738
ShreyashSonone <104378762+ShreyashSonone@users.noreply.github.com>
2022-08-17 05:18:18 -0400

    Add files via upload

---
+++ NastedIf.java
@@ -0,0 +1,29 @@
+/*
+Program: NastedIf
+@Author: Shreyash Sonone
+@Date: 17 Aug 2022
+*/
+
+class NestedIf
+{
+	static void takeAdmission()
+	{
+		float totalMarks=73.9f;
+		float pcmMarks=87.9f;
+		if(totalMarks>75)
+		{
+			if(pcmMarks>85)
+				System.out.println("---You are eligible for our college(B.Tech)---");
+			else
+				System.out.println("---You are eligible for our college(BCA)---");
+		}
+		else
+			System.out.println("---You are not eligible for our college---");
+		System.out.println("---Thank You for Applying---");
+	}	
+	public static void main(String args[])
+	{
+		takeAdmission();
+		
+	}
+}	
